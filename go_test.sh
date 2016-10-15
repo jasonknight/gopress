5459fb929161e78f4fc053b89c8e9f0a7305c6c8
Jason Martin <jason@thebigrede.net>
2016-10-15 16:21:07 +0200

    All models creating, saving, and finding

--- go_test.sh
+++ go_test.sh
@@ -2,7 +2,7 @@
 
 set -e
 echo "" > coverage.txt
-go test -race -coverprofile=profile.out -covermode=atomic $d
+go test -race -coverprofile=profile.out -covermode=count $d
 if [ -f profile.out ]; then
     cat profile.out >> coverage.txt
     rm profile.out
